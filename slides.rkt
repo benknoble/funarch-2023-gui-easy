bd9ba3c583a2f356b131981dd9c12f37d388618b
D. Ben Knoble <ben.knoble+github@gmail.com>
2023-09-01 10:45:57 -0400

    fin

--- slides.rkt
+++ slides.rkt
@@ -1,6 +1,7 @@
 #lang at-exp slideshow
 
-(require slideshow/code
+(require slideshow/text
+         slideshow/code
          slideshow/step
          slideshow/play
          racket/runtime-path
@@ -8,9 +9,12 @@
          racket/draw
          racket/class
          pict/face
+         syntax/parse/define
          qi)
 
 (define-runtime-path here ".")
+
+(define arrow-size 10)
 
 (define (color-code col p)
   (define q (inset p 2))
@@ -19,9 +23,87 @@
   (~> (w h 5) filled-rounded-rectangle
       (colorize col) (cc-superimpose p) (refocus p)))
 
+(define-syntax interact-gui-easy
+  (syntax-parser
+   [(_ {~optional {~seq #:alt alt:expr}}
+       e:expr ... t:expr)
+    #`(interactive
+       {~? alt (blank)}
+       (λ (f)
+         #,(syntax-local-introduce #'(local-require racket/gui/easy
+                                                    racket/gui/easy/operator))
+         e ...
+         (embed f t)
+         void))]))
+
+(define (strikethrough p)
+  (define line
+    (~> (p) (-< pict-width pict-height) hline))
+  (cc-superimpose p line))
+
+(define column
+  (ghost
+   (rectangle (/ (pict-width titleless-page) 2)
+              (pict-height titleless-page))))
+
+(define 1/3column
+  (ghost
+   (rectangle (/ (pict-width titleless-page) 3)
+              (pict-height titleless-page))))
+
+(define 2/3column (hc-append 1/3column 1/3column))
+
+(define editor-widget-text
+  @text{Editor})
+(define editor-widget
+  (frame (inset editor-widget-text 20)))
+
+(define input-view
+  (frame (inset @small{@t{input%}} 20)))
+
+(define pic-widget-text
+  (let ([label (inset @text{Picture} 20)])
+    (color-code "lightcoral" label)))
+(define pic-widget
+  (frame (inset pic-widget-text 20)))
+
+(define pic-view
+  (frame (inset (inset @small{@t{pict-canvas%}} 20)
+                20)))
+
+(define editor-and-pic-widget
+  (hc-append editor-widget pic-widget))
+
+(define hpanel-view
+  (frame (lt-superimpose
+          (inset (hc-append input-view pic-view) 25)
+          (translate @small{@t{hpanel%}} 20 0))))
+
+(define example-window-widget
+  (frame
+   (vc-append
+    (let* ([height (pict-height @text{AoE Editor})]
+           [buttons (hc-append 3
+                               (disk (- height 5) #:color "red" #:draw-border? #f)
+                               (disk (- height 5) #:color "yellow" #:draw-border? #f)
+                               (disk (- height 5) #:color "green" #:draw-border? #f))])
+      (lc-superimpose (cc-superimpose (filled-rectangle (pict-width editor-and-pic-widget)
+                                                        height
+                                                        #:color "light gray")
+                                      @text{AoE Editor})
+                      (inset buttons 2)))
+    editor-and-pic-widget)))
+
+(define example-window-view
+  (frame (lt-superimpose
+          (inset hpanel-view 25)
+          (translate @small{@t{window%}} 20 0))))
+
 (slide
  #:title "Functional Shell and Reusable Components for Easy GUIs"
- @t{D. Ben Knoble & Bogdan Popa})
+ (~> (titleless-page)
+     (ct-superimpose @small{@t{and some Frosthaven}})
+     (cc-superimpose @t{D. Ben Knoble & Bogdan Popa})))
 
 (define typ-solution
   @t{Typical Solution: object-based toolkit})
@@ -32,54 +114,50 @@
       [happy (~> ((face 'happy)) (scale-to-fit our-solution))])
   (slide
    #:title "Problem: Let's make a small GUI tool"
+   'next
    'alts
    (list
+    (list @small{@t{(with Racket)}})
     (list (hc-append 5 (ghost sad) typ-solution (ghost sad)))
     (list
      @t{Working with the object-based toolkit:}
      'alts
-     (list (list @item[#:bullet (colorize @tt{x} "red")]{Code not organized like the GUI})
+     (list (list @item[#:bullet (colorize @tt{x} "red")]{Code not organized like GUI})
            (list @item[#:bullet (colorize @tt{x} "red")]{Application state and GUI state manually synchronized})
-           (list @item[#:bullet (colorize @tt{x} "red")]{Composition and reuse thwarted by construction order})
+           (list @item[#:bullet (colorize @tt{x} "red")]{Composition, reuse, abstraction thwarted by construction})
            (list @item[#:bullet (colorize @tt{x} "red")]{Daunting: requires expertise in the widget library})
            (list @it{Experts and beginners alike struggle})))
-    (list (hc-append 5 (ghost sad) typ-solution (ghost sad)))
     (list (hc-append 5 sad typ-solution sad))
+    (list
+     @t{Working with GUI Easy:}
+     'alts
+     (list (list @item[#:bullet (colorize @tt{✓} "green")]{GUI containers programatically contain GUI children}
+                 (strikethrough @item[#:bullet (colorize @tt{x} "red")]{Code not organized like the GUI}))
+           (list @item[#:bullet (colorize @tt{✓} "green")]{Views update automatically when observables change}
+                 (strikethrough @item[#:bullet (colorize @tt{x} "red")]{Application state and GUI state manually synchronized}))
+           (list @item[#:bullet (colorize @tt{✓} "green")]{GUIs are composable functions}
+                 (strikethrough @item[#:bullet (colorize @tt{x} "red")]{Composition and reuse thwarted by construction}))
+           (list @item[#:bullet (colorize @tt{✓} "green")]{Small flexible core}
+                 @item[#:bullet (colorize @tt{✓} "green")]{Easy to extend}
+                 (strikethrough @item[#:bullet (colorize @tt{x} "red")]{Daunting: requires expertise in the widget library}))
+           (list @it{Easier to learn}
+                 @it{Gateway to object-oriented toolkit})))
     (list (hc-append 5 happy our-solution happy)))))
 
 (slide
  #:title "Example: Area of Effect Diagram Editor"
  @t{Goal}
- (let ([editor (hc-append (frame (inset @text{Editor} 20))
-                          (frame (inset (let ([label (inset @text{Picture} 20)])
-                                          (color-code "lightcoral" label))
-                                        20)))])
-   (scale (frame
-           (vc-append
-            (let* ([height (pict-height @text{AoE Editor})]
-                   [buttons (hc-append 3
-                                       (disk (- height 5) #:color "red" #:draw-border? #f)
-                                       (disk (- height 5) #:color "yellow" #:draw-border? #f)
-                                       (disk (- height 5) #:color "green" #:draw-border? #f))])
-              (lc-superimpose (cc-superimpose (filled-rectangle (pict-width editor)
-                                                                height
-                                                                #:color "light gray")
-                                              @text{AoE Editor})
-                              (inset buttons 2)))
-            editor))
-          2)))
+ (scale example-window-widget 2))
 
 ;; runnable example code here
 
 (void
  (with-steps
   {code main-func state-sync boilerplate all}
-  (define-syntax only-on-after
-    (syntax-rules ()
-      [(_ on af then else)
-       (if (or (only? on) (only? af) (after? af))
-         then
-         else)]))
+  (define-syntax-parse-rule (only-on-after on:id af:id then:expr else:expr)
+    (if (or (only? on) (only? af) (after? af))
+      then
+      else))
   (define (color-main x) (only-on-after main-func all (color-code "aquamarine" x) x))
   (define (color-state x) (only-on-after state-sync all (color-code "gold" x) x))
   (define (color-boilerplate x) (only-on-after boilerplate all (color-code "lightcoral" x) x))
@@ -145,13 +223,7 @@
        (if (>= 1.0)
          (gen (last editor-frames))
          (~>> (* n-editor-frames) exact-floor (list-ref editor-frames)))))
-   (define bitmap
-     (make-object bitmap% frame-file 'png))
-   (define-values (w h)
-     (values (send bitmap get-width) (send bitmap get-height)))
-   (~> ((dc (λ (dc dx dy)
-              (send dc draw-bitmap bitmap dx dy))
-            w h))
+   (~> (frame-file) bitmap
        (scale-to-fit titleless-page)
        (cc-superimpose titleless-page)
        (rb-superimpose (clickback (frame (t "Run OOP Code")) run-oop))))
@@ -165,7 +237,7 @@
 (void
  (with-steps
   {render window hpanel input pict-canvas run}
-  (define-syntax-rule (reveal id then)
+  (define-syntax-parse-rule (reveal id:id then:expr)
     (let ([then* (code then)])
       (before id (ghost then*) (only id (color-code "aquamarine" then*) then*))))
   (define (run-easy)
@@ -203,25 +275,281 @@
        (cc-superimpose titleless-page)
        (rb-superimpose ((vonly run) (clickback (frame (t "Run Code")) run-easy)))))))
 
+(slide
+ #:title "Easy Example: Area of Effect Diagram Editor"
+ (interact-gui-easy
+  #:alt titleless-page
+  (local-require (prefix-in pict: pict)
+                 frosthaven-manager/aoe-images)
+  (define |@|input (obs ""))
+  (hpanel
+   (input |@|input
+          (λ (_action input)
+            (:= |@|input input))
+          #:style '(multiple)
+          #:stretch '(#t #t)
+          #:min-size '(300 150))
+   (pict-canvas |@|input
+                (λ (input)
+                  (with-handlers ([exn:fail? (λ (_exn) (pict:text "Invalid Input"))])
+                    (spec->shape (string->spec input))))
+                #:min-size '(300 150)))))
+
 ;; ---
 
 (slide #:name "GUI Easy Details"
        @titlet{GUI Easy Details})
 
-;; Can I macro-ize this, or otherwise librarify it so that it is easy to embed
-;; GUI Easy examples?
-(slide
- (interactive
-  @t{"Wee"}
-  (λ (f)
-    (local-require racket/gui/easy)
-    (define count (obs 0))
-    (embed f
-           (hpanel (button "-" (λ () (obs-update! count sub1)))
-                   (text (obs-map count ~a))
-                   (button "+" (λ () (obs-update! count add1)))))
-    void)))
-
 ;; GUI Easy: the library
+(slide
+ #:title "GUI Easy Details"
+ (ct-superimpose
+  @t{Observables are Values}
+  (hc-append
+   (cc-superimpose column
+                   (vr-append gap-size
+                              @t{Observables}
+                              @t{Set Observables}
+                              @t{Update Observables}
+                              @t{Map Observables}
+                              @t{Peek Observables}
+                              @t{Subscribe}))
+   (cc-superimpose column
+                   (vc-append gap-size
+                              ;; ghosts for alignment
+                              (code #,(ghost (code x<~)) (|@| _v) #,(ghost (code _fx)))
+                              (code (:= (|@| _v) _e))
+                              (code (<~ (|@| _v) _f))
+                              (code (~> (|@| _v) _f))
+                              (code (obs-peek (|@| _v)) #,(ghost (code xxxxxxx)))
+                              (code obs-subscribe!))))))
+
+(slide
+ #:title "GUI Easy Details"
+ (~> (titleless-page)
+     (ct-superimpose @t{Views are Functions})
+     (cc-superimpose (code (-> ... view<%>)))))
+
+(slide
+ #:title "GUI Easy Details"
+ (~> (titleless-page)
+     (ct-superimpose @para[#:align 'center]{Views are @it{Observable-aware} Functions})
+     (cc-superimpose
+      (vl-append
+       gap-size
+       (code (-> _dependencies view<%>))
+       (code (pict-canvas _|@|data _draw-pict-from-data))))))
+
+(slide
+ #:title "GUI Easy Details"
+ (~> (titleless-page)
+     (ct-superimpose @para[#:align 'center]{Views are @it{Composable} Functions})
+     (cc-superimpose
+      (vl-append
+       gap-size
+       (code (-> _children view<%>))
+       (code (hpanel _child1 _child2 _child3))))))
+
+(slide
+ #:title "GUI Easy Details"
+ (hc-append
+  (cc-superimpose 2/3column
+                  (scale-to-fit (code
+                                 (define |@|count (|@| 0))
+                                 code:blank
+                                 (render
+                                  (window
+                                   (hpanel
+                                    (button "-" (λ () (<~ |@|count sub1)))
+                                    (text (~> |@|count number->string))
+                                    (button "+" (λ () (<~ |@|count add1)))))))
+                                2/3column))
+  (cc-superimpose 1/3column
+                  (interact-gui-easy
+                   (define |@|count (|@| 0))
+                   (hpanel (button "-" (λ () (<~ |@|count sub1)))
+                           (text (~> |@|count number->string))
+                           (button "+" (λ () (<~ |@|count add1))))))))
+
+(slide #:name "Abstraction"
+       @titlet{Abstraction})
+
+(slide
+ #:title "Abstraction"
+ (hc-append
+  (cc-superimpose 2/3column
+                  (scale-to-fit (code
+                                 (define (counter |@|count action)
+                                   (hpanel
+                                    (button "-" (λ () (action sub1)))
+                                    (text (~> |@|count number->string))
+                                    (button "+" (λ () (action add1)))))
+                                 code:blank
+                                 (define |@|count1 (|@| 0))
+                                 (define |@|count2 (|@| 0))
+                                 code:blank
+                                 (render
+                                  (window
+                                   (vpanel
+                                    (counter |@|count1 (λ (proc) (<~ |@|count1 proc)))
+                                    (counter |@|count2 (λ (proc) (<~ |@|count2 proc)))))))
+                                2/3column))
+  (cc-superimpose 1/3column
+                  (interact-gui-easy
+                   (define (counter |@|count action)
+                     (hpanel
+                      (button "-" (λ () (action sub1)))
+                      (text (~> |@|count number->string))
+                      (button "+" (λ () (action add1)))))
+                   (define |@|count1 (|@| 0))
+                   (define |@|count2 (|@| 0))
+                   (vpanel
+                    (counter |@|count1 (λ (proc) (<~ |@|count1 proc)))
+                    (counter |@|count2 (λ (proc) (<~ |@|count2 proc))))))))
+
 ;; GUI Easy: the architecture (via Frosthaven Manager)
-;; Research connections
+
+(slide #:name "Model-View-Controller"
+       @titlet{Model-View-Controller})
+
+(void
+ (with-steps
+  {blank model view controller}
+  (define-syntax-parse-rule (highlight id:id c:expr ...)
+    (let ([c* (code c ...)])
+      (if (only? id) (color-code "aquamarine" c*) c*)))
+  (slide
+   #:title "Model-View-Controller"
+   (cond
+     [(only? model) @t{Model}]
+     [(only? view) @t{View}]
+     [(only? controller) @t{Controller}]
+     [else @t{ }])
+
+   (scale-to-fit
+    (code
+     #,(highlight
+        view
+        (define (counter |@|count #,(highlight controller action))
+          (hpanel
+           (button "-" #,(highlight controller (λ () (action sub1))))
+           (text (~> |@|count number->string))
+           (button "+" #,(highlight controller (λ () (action add1)))))))
+     code:blank
+     #,(highlight
+        model
+        (define |@|count1 (|@| 0))
+        (define |@|count2 (|@| 0)))
+     code:blank
+     (render
+      #,(highlight
+         view
+         (window
+          (vpanel
+           (counter |@|count1 #,(highlight controller (λ (proc) (<~ |@|count1 proc))))
+           (counter |@|count2 #,(highlight controller (λ (proc) (<~ |@|count2 proc)))))))))
+    titleless-page))))
+
+(slide
+ #:title "Model-View-Controller"
+ @para{Pass observable models from top of application down to child components})
+
+(slide
+ #:title "Model-View-Controller"
+ @para{Controller callbacks specialize components to react to user input})
+
+(slide
+ #:title "Model-View-Controller"
+ @it{Data Down, Actions Up (DDAU)}
+ @it{Reusable Components ~ Pure Functions})
+
+(slide
+ #:title "Other Details (in the paper)"
+ @item{Object lifecyle managed by @code[view<%>] and renderer objects}
+ @subitem{Inversion of Control}
+ @item{Black-box vs. White-box frameworks}
+ @subitem{Escape hatches: custom @code[view<%>]s, @it{mixins}}
+ @item{Related work: GUI toolkits, reactive web frameworks, FRP})
+
+(slide
+ #:title "Conclusion"
+ @t{Functional Shell and Reusable Components for Easy GUIs}
+ @item{GUIs with objects: hard}
+ @item{GUIs with observables and functions: easier, FP}
+ @item{Purity, composition, abstraction, reuse!}
+ @item{What other imperative systems deserve the FP treatment?})
+
+(slide
+ #:title "Extras"
+ (~> ("screenshot-frosthaven.png")
+     bitmap
+     (scale-to-fit titleless-page)
+     (cc-superimpose titleless-page)))
+
+(slide
+ #:title "Extras"
+ (~> ("screenshot-frosthaven-with-server.png")
+     bitmap
+     (scale-to-fit titleless-page)
+     (cc-superimpose titleless-page)))
+
+(slide
+ #:title "Extras"
+ 'alts
+ (let ([diagram (hc-append (cc-superimpose column (scale example-window-widget 1.5))
+                           (cc-superimpose column example-window-view))])
+   (define alts
+     (~> (diagram)
+         (-< (pin-arrows-line arrow-size _
+                              example-window-view lt-find
+                              example-window-widget rt-find)
+             (pin-arrows-line arrow-size _
+                              hpanel-view lt-find
+                              editor-and-pic-widget rt-find)
+             (pin-arrows-line arrow-size _
+                              input-view lt-find
+                              editor-widget-text rt-find)
+             (pin-arrows-line arrow-size _
+                              pic-view lt-find
+                              pic-widget-text rt-find)) collect (map list _)))
+   alts))
+
+(slide
+ #:title "Extras"
+ @t{Create}
+ (let* ([window% (frame (inset @code[window%] 5))]
+        [hpanel% (frame (inset @code[hpanel%] 5))]
+        [input% (frame (inset @code[input%] 5))]
+        [pict-canvas% (frame (inset @code[pict-canvas%] 5))]
+        [diag (vc-append gap-size
+                        window%
+                        hpanel%
+                        (hc-append gap-size
+                                   input%
+                                   pict-canvas%))])
+   (~> (diag)
+       (pin-arrow-line arrow-size _
+                       window% cb-find
+                       hpanel% ct-find)
+       (pin-arrow-line arrow-size _
+                       hpanel% cb-find
+                       input% ct-find)
+       (pin-arrow-line arrow-size _
+                       hpanel% cb-find
+                       pict-canvas% ct-find))))
+
+(slide
+ #:title "Extras"
+ (~> (example-window-view)
+     (pin-arrow-line arrow-size _
+                     input-view lt-find
+                     example-window-view lt-find
+                     #:label @small{@t{deps}}
+                     #:x-adjust-label -60
+                     #:y-adjust-label -20)
+     (pin-arrow-line arrow-size _
+                     example-window-view rt-find
+                     pic-view rt-find
+                     #:label @small{@t{updates}}
+                     #:x-adjust-label +70
+                     #:y-adjust-label -10)))
